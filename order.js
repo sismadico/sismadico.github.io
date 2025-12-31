f0c9610365135678a251826119c50950335bb80a
Sismadi Langit Solusi <36598164+sismadico@users.noreply.github.com>
2025-12-31 15:22:29 +0700

    Add files via upload

--- order.js
+++ order.js
@@ -373,25 +373,25 @@ Message *
 
 
 
-// const burger = document.getElementById('burgerBtn');
-// const nav = document.getElementById('navLinks');
+const burger = document.getElementById('burgerBtn');
+const nav = document.getElementById('navLinks');
 
-// // Toggle menu
-// burger.addEventListener('click', (e) => {
-//   nav.classList.toggle('active');
-//   e.stopPropagation(); // prevent event bubbling
-// });
+// Toggle menu
+burger.addEventListener('click', (e) => {
+  nav.classList.toggle('active');
+  e.stopPropagation(); // prevent event bubbling
+});
 
-// // Hide menu when clicking outside
-// document.addEventListener('click', (e) => {
-//   if (nav.classList.contains('active') && !nav.contains(e.target) && e.target !== burger) {
-//     nav.classList.remove('active');
-//   }
-// });
+// Hide menu when clicking outside
+document.addEventListener('click', (e) => {
+  if (nav.classList.contains('active') && !nav.contains(e.target) && e.target !== burger) {
+    nav.classList.remove('active');
+  }
+});
 
-// // Optional: Hide menu after clicking a menu link
-// nav.querySelectorAll('a').forEach(link => {
-//   link.addEventListener('click', () => {
-//     nav.classList.remove('active');
-//   });
-// });
+// Optional: Hide menu after clicking a menu link
+nav.querySelectorAll('a').forEach(link => {
+  link.addEventListener('click', () => {
+    nav.classList.remove('active');
+  });
+});
